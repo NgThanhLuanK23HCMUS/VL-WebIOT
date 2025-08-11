d70165b1f0596577bd5f70e47402175fd5854118
NgThanhLuanK23HCMUS <ntluan23@clc.fitus.edu.vn>
2025-08-11 11:19:08 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,6 @@ void runAutoModeApp(){
     if (sendSensorDataHandler != nullptr){
 
       sendSensorDataHandler->send(dataUrl);
-      // sendSensorDataHandler->sendToThingSpeak();
     } 
     
   }
@@ -89,7 +88,6 @@ void runAutoModeApp(){
   shockSensor->readSensorData();
   if(!shockSensor->getIsShock()){
     // sendSensorDataHandler->sendShockData(shockDataUrl);
-    // sendSensorDataHandler->sendCurrentTimeToThingSpeak(5);
   }
 
 }
@@ -129,7 +127,6 @@ void runManualModeApp() {
     if (millis() - lastPumpTime >= pumpInterval) {
       relay->turnOff();  
       relay->setIsOn(false); 
-      sendSensorDataHandler->sendCurrentTimeToThingSpeak(4);
     }
     else{
 
