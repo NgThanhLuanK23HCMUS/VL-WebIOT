4f00d57c24ee544ab39247cda4b0c402f538aefe
NgThanhLuanK23HCMUS <ntluan23@clc.fitus.edu.vn>
2025-08-11 11:38:17 +0700

    update send data thread

--- app.py
+++ app.py
@@ -52,5 +52,6 @@ def get_data():
 
 if __name__ == "__main__":
     thread = threading.Thread(target=cl.send_data_loop, daemon=True)
+    thread.start()
     app.run(host="0.0.0.0", port=5000, debug=True)
 
